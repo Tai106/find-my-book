8e02c89489034c00e94451c40bdb0712e550b8c2
Gravity <tevoned@gmail.com>
2022-06-01 21:21:33 -0400

    mutations

--- find-my-book/client/src/utils/localStorage.js
+++ find-my-book/client/src/utils/localStorage.js
@@ -2,11 +2,13 @@ export const getSavedBookIds = () => {
   const savedBookIds = localStorage.getItem('saved_books')
     ? JSON.parse(localStorage.getItem('saved_books'))
     : [];
+  console.log(savedBookIds);
 
   return savedBookIds;
 };
 
 export const saveBookIds = (bookIdArr) => {
+  console.log(bookIdArr);
   if (bookIdArr.length) {
     localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
   } else {
@@ -15,10 +17,11 @@ export const saveBookIds = (bookIdArr) => {
 };
 
 export const removeBookId = (bookId) => {
+  console.log(bookId);
   const savedBookIds = localStorage.getItem('saved_books')
     ? JSON.parse(localStorage.getItem('saved_books'))
     : null;
-
+  console.log(savedBookIds);
   if (!savedBookIds) {
     return false;
   }
